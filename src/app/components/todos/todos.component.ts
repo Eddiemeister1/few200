f482315ea356164f805901213782dfd72b5079d2
Eduardo Dotel <42874513+Eddiemeister1@users.noreply.github.com>
2021-06-28 18:24:52 +0000

    Todos and List Development in progress

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TodoListItemViewModel } from 'src/app/models/todos.models';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+  todoList: TodoListItemViewModel[] = [
+    { id: '1', description: 'Rake Leaves' },
+    { id: '2', description: 'Wash Deck' }
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onItemAdded(thing: { item: string }) {
+    const newItem: TodoListItemViewModel = {
+      description: thing.item,
+      id: '99'
+    };
+
+    this.todoList = [newItem, ...this.todoList];
+    console.log(thing);
+  }
 }
